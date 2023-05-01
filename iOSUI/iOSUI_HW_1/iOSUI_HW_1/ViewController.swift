0b11335a8c4c3883121a6141e0273209da4add1d
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-05-01 14:02:51 +0300

    Started working on iOS UI Homework 1

---
+++ iOSUI/iOSUI_HW_1/iOSUI_HW_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSUI_HW_1
+//
+//  Created by Philipp Lazarev on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
