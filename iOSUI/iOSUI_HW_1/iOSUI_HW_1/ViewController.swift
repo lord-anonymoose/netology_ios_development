76a1a1c2b534b90695662ecbe2e94c0b1d61c01f
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-05-03 12:23:22 +0300

    Used SF Symbol in iOSUI HW1

--- iOSUI/iOSUI_HW_1/iOSUI_HW_1/ViewController.swift
+++ iOSUI/iOSUI_HW_1/iOSUI_HW_1/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let textColor = UIColor(named: "textColor")
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameView: UILabel!
+    
+    @IBOutlet weak var birthdateView: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var cityView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameView.text = "Tim Cook"
+        birthdateView.text = "November 1st, 1960"
+        
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .semibold)
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: "location", withConfiguration: symbolConfig)?.withTintColor(textColor ?? .black)
+
+        let imageString = NSMutableAttributedString(attachment: attachment)
+        let textString = NSAttributedString(string: "Cupertino")
+        imageString.append(textString)
+
+        cityView.attributedText = imageString
+        cityView.sizeToFit()
     }
 
 
